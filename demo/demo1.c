b226f6ccae494196d8973d1df04005e84fc98f42
mhhieu2385 <hoangminhhieu230805@gmail.com>
2023-08-27 23:21:44 +0700

    #

---
+++ demo/demo1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+    int a, b, c, d;
+    a = 50;
+    b = 24;
+    c = 68;
+    d = a*b+c/2;
+    printf("\n The value after a*b+c/2 is : %d", d);
+    //d = a%b;
+
+    return 0;
+}
