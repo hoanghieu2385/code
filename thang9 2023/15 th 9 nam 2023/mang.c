4ec5e73e38186ad197e51224b4b29886259abba4
hoanghieu2385 <hoangminhhieu230805@gmail.com>
2023-09-15 11:29:58 +0700

    No Comment

--- thang9 2023/15 th 9 nam 2023/mang.c
+++ thang9 2023/15 th 9 nam 2023/mang.c
@@ -2,29 +2,22 @@
 #include <math.h>
 void main(){
     int arr[5];
-    int i, max, min, sum, avg;
+    int i, min, sum, avg;
     sum = 0;
     for (i = 0; i < 5; i++)
     {
         printf("Enter number %d: ", i+1 );
-        scanf("%d", &arr);
+        scanf("%d", &arr[i]);
         sum += arr[i];
     }
-    max = arr[0];
-    for (i = 0; i < 5; i+1)
-    {
-        if ( arr[i] > max)
-        max = arr[i];
-    }
     min = arr[0];
-    for ( i = 0; i < 5; i+1)
+    for ( i = 1; i < 5; i++)
     {
         if ( arr[i] < min)
         min = arr[i];
     }
-    
-
-    printf("\nThe number max is: %d", max);
     printf("\nThe number min is: %d", min);
+    avg = sum/5;
     printf("\nThe sum is: %d", sum);
+    printf("\nThe avg is: %d", avg);
 }
