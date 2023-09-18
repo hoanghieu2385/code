e09640e38c62995884d10fb74755797153ed10d8
hoanghieu2385 <hoangminhhieu230805@gmail.com>
2023-09-18 10:02:26 +0700

    No Comment

---
+++ thang9 2023/18 th 9 nam 2023/menuquan.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+void menu();
+void menu(){
+    printf("\nDo uong: ");
+    printf("\n1, Tra chanh");
+    printf("\n2, Tra dao cam xa");
+    printf("\n3, Cafe");
+    printf("\n4, Tra sua");
+    printf("\nDo an: ");
+    printf("\n1, Banh mi pate");
+    printf("\n2, Bong lan trung muoi");
+    printf("\n3, Socola");
+    printf("\n4, Xuc xich");
+}
+void main(){
+    printf("\n*********  Lan 1     *********");
+    menu();
+    printf("\n*********  Lan 2     *********");
+    menu();
+}
---
+++ thang9 2023/18 th 9 nam 2023/traitym.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+void hcn();
+void hcn(){
+    printf("       **    ** ");
+    printf("\n    **    **    **");
+    printf("\n    **          **");
+    printf("\n      **      **");
+    printf("\n        **  **");
+    printf("\n          ** \n");
+    }
+void main(){
+    printf("Lan 1\n");
+    hcn();
+    printf("Lan 2\n");
+    hcn();
+    printf("Lan 3\n");
+    hcn();
+
+}
