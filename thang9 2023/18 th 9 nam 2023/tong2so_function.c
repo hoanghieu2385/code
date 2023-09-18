319cc969452c21f73c681a54ac16ec7faeae912b
hoanghieu2385 <hoangminhhieu230805@gmail.com>
2023-09-18 11:02:16 +0700

    No Comment

---
+++ thang9 2023/18 th 9 nam 2023/tong2so_function.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int add(int a, int b);
+int main(){
+    int result = add(5, 3);
+    printf("Tong la: %d\n", result);
+
+    int result2 = add(4, 3);
+    printf("Tong la: %d\n", result2);
+
+    int num1, num2;
+    printf("Nhap so hang thu nhat: ");
+    scanf("%d", &num1);
+    printf("Nhap so hang thu nhat: ");
+    scanf("%d", &num2);
+
+    int result3 = add(num1, num2);
+    printf("Tong la: %d\n", result3);
+    return 0;
+}
+
+int add(int a, int b) {
+    printf("\n\nThuc hien tinh cong: \n");
+    printf("So hang a = %d\n", a);
+    printf("So hang b = %d\n", b);
+    return a + b;
+}
