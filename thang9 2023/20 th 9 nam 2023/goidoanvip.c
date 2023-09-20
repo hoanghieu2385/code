472b6c6b28237896d66597d40510a4bfbeed94e3
hoanghieu2385 <hoangminhhieu230805@gmail.com>
2023-09-20 11:17:59 +0700

    No comment

---
+++ thang9 2023/20 th 9 nam 2023/goidoanvip.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+
+void monan();
+void menu(char luachon);
+float bill(char luachon, char soluong);
+
+int main()
+{
+    char luachon;
+    char soluong;
+
+    do {
+        monan();
+        printf("Nhap lua chon cua ban: ");
+        scanf("%c", &luachon);
+        if ( luachon != 'd' )
+        {
+            printf("Nhap so luong do an ban muon mua: ");
+            scanf("%d", &soluong);
+            menu(luachon);
+            float tongtien = bill(luachon, soluong);
+            printf("Cua ban het: $%.3f\n\n", tongtien);
+        }
+    } while (luachon != 'd');
+    return 0;
+}
+
+void monan()
+{
+    printf("********** Quan An VIPP **********\n");
+    printf("a. Banh mi xuc xich\n");
+    printf("b. Banh mi pate\n");
+    printf("c. Banh mi cha ca\n");
+    printf("d. Quit\n");
+}
+
+void menu(char luachon)
+{
+    switch ( luachon ) {
+        case 'a':
+            printf("Ban da oder Banh mi xuc xich\n");
+            break;
+        case 'b':
+            printf("Ban da oder Banh mi pate\n");
+            break;
+        case 'c':
+            printf("Ban da oder Banh mi cha ca\n");
+            break;
+        default:
+            printf("Gia tri khong hop le. Vui long thu lai.\n");
+    }
+}
+
+float bill(char luachon, char soluong)
+{
+    float giatien;
+
+    switch (luachon)
+    {
+    case 'a':
+        giatien = 20;
+        break;
+    case 'b':
+        giatien = 15;
+        break;
+    case 'c':
+        giatien = 10;
+        break; 
+    default:
+        giatien = 0;
+    }
+    return giatien * soluong;
+}
